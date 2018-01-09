dd3187ddd4c0d9923f3976330e6a4e31c9c562b1
alexandrmorgun93 <alexandrmorgun93@gmail.com>
2018-01-09 08:19:11 +0200

    lo

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
-;
-(() => {
-  'use strict';
-  
-  /*variables*/
+/*BEGIN PARALLAX*/
+const Parallax = require('scroll-parallax');
+let p = new Parallax('.parallax', {
+    intensity: 40
+}).init();
 
-
-
-})();
+/*END PARALLAX*/
